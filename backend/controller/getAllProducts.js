b19bd9574be5de98779febc70bf993db196a9742
Jean Urena <Jean.Urena@911.gob.do>
2022-10-06 09:49:17 -0400

    Customer CRUD FULLY

--- backend/controller/getAllProducts.js
+++
@@ -1,9 +0,0 @@
-const connection = require('../connection/db');
-
-connection.query(
-    'SELECT * FROM `Persons`',
-    function(err, results, fields) {
-      console.log(results[0]); // results contains rows returned by server
-  // fields contains extra meta data about results, if available
-    }
-  );
--- backend/controller/newCustomer.js
+++
@@ -1,9 +0,0 @@
-const connection = require('../connection/db');
-
-connection.query(
-    'INSERT INTO `Customer`(FIRST_NAME,LAST_NAME, DATE_OF_BIRTH, ADDRESS, PHONE_NUMBER, EMAIL_ADDRESS) values ()',
-    function(err, results, fields) {
-      console.log(results[0]); // results contains rows returned by server
-  // fields contains extra meta data about results, if available
-    }
-  );
